55881661b0f975c6d0f74167452a8a9455301d35
Mhirii <ahmedmhiri218@gmail.com>
2025-05-11 21:15:35 +0100

    access/init

---
+++ access/internal/svc/servicecontext.go
@@ -0,0 +1,13 @@
+package svc
+
+import "github.com/tempest-org/tempest/access/internal/config"
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
